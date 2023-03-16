e077f82324f17cf288c55159fb8cc1215f3ef185
bhatshakran <bhatshakran@gmail.com>
2023-03-16 22:06:31 +0530

    View clients created

--- src/routes/Dashboard/Clients/Clients.jsx
+++ src/routes/Dashboard/Clients/Clients.jsx
@@ -1,85 +1,269 @@
-import { Space, Table, Tag } from 'antd';
-import React from 'react';
+import { Space, Switch, Table } from 'antd';
+import { Dropdown } from 'antd';
+import React, { useState } from 'react';
 import HeaderOfTable from './TableHeader/HeaderOfTable';
+import { BiDotsVerticalRounded } from 'react-icons/bi';
+import infosys from '../../../assets/infosys.png';
+import wipro from '../../../assets/wipro.png';
+import accenture from '../../../assets/accenture.png';
+import hcl from '../../../assets/hcl.png';
+import lt from '../../../assets/lt.png';
+import redingiton from '../../../assets/redingiton.png';
+import lodha from '../../../assets/lodha.png';
+import cape from '../../../assets/cape.png';
+import tat from '../../../assets/tat.png';
+import mphasis from '../../../assets/mphasis.png';
+
+const avatarArr = [
+  infosys,
+  wipro,
+  accenture,
+  cape,
+  lodha,
+  tat,
+  hcl,
+  redingiton,
+  mphasis,
+  lt,
+];
+
+const items = [
+  {
+    key: '1',
+    label: 'Action 1',
+  },
+  {
+    key: '2',
+    label: 'Action 2',
+  },
+];
 
 const columns = [
   {
-    title: 'Name',
+    title: 'Company Name',
     dataIndex: 'name',
     key: 'name',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
-      <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
+    render: (text, record) => (
+      <span className='nunito bluishblack f-14 d-flex gap-1'>
+        <img src={avatarArr[record.key]} alt='' width={'20px'} height='20px' />
+        {text}
+      </span>
     ),
   },
   {
-    title: 'Action',
+    title: 'Email Address',
+    dataIndex: 'email',
+    key: 'email',
+    render: (text) => <span className='nunito bluishblack f-14'>{text}</span>,
+  },
+  {
+    title: 'Phone No',
+    dataIndex: 'phone',
+    key: 'phone',
+    render: (text) => <span className='nunito bluishblack f-14'>{text}</span>,
+  },
+  {
+    title: 'Contact Person',
+    key: 'contact',
+    dataIndex: 'contact',
+    render: (text) => <span className='nunito bluishblack f-14'>{text}</span>,
+  },
+  {
+    title: 'Facilitator',
+    key: 'facilitator',
+    dataIndex: 'facilitator',
+    render: (text) => <span className='nunito bluishblack f-14'>{text}</span>,
+  },
+  {
+    title: 'Sites',
+    key: 'sites',
+    dataIndex: 'sites',
+    render: (text) => <span className='nunito bluishblack f-14'>{text}</span>,
+  },
+
+  {
+    title: 'Tenants',
+    key: 'tenants',
+    dataIndex: 'tenants',
+    render: (text) => <span className='nunito bluishblack f-14'>{text}</span>,
+  },
+  {
+    title: 'Tenant Groups',
+    key: 'tgroups',
+    dataIndex: 'tgroups',
+    render: (text) => <span className='nunito bluishblack f-14'>{text}</span>,
+  },
+
+  {
+    title: 'Actions',
     key: 'action',
-    render: (_, record) => (
-      <Space size='middle'>
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
+    dataIndex: 'actions',
+    render: () => (
+      <div className='d-flex align-items-center justify-content-center fs-5'>
+        <Dropdown
+          menu={{
+            items,
+          }}
+        >
+          <span role='button'>
+            <BiDotsVerticalRounded />
+          </span>
+        </Dropdown>
+      </div>
     ),
   },
 ];
 const data = [
   {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
+    key: '0',
+    name: 'Infosys',
+    email: 'management@infosys.com',
+    phone: '+91 95963214587',
+    contact: 'Vijayent Roy',
+    sites: '12',
+    facilitator: '--',
+    tenants: '--',
+    tgroups: '--',
   },
+
+  {
+    key: '1',
+    name: 'Wipro',
+    email: 'management@wipro.com',
+    phone: '+91 95963214587',
+    contact: 'Kalpit Soni',
+    sites: '8',
+    facilitator: '--',
+    tenants: '--',
+    tgroups: '--',
+  },
+
   {
     key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
+    name: 'Accenture',
+    email: 'management@accenture.com',
+    phone: '+91 95963214587',
+    contact: 'Drishti Sane',
+    sites: '4',
+    facilitator: '--',
+    tenants: '--',
+    tgroups: '--',
   },
   {
     key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sydney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
+    name: 'Capgemini',
+    email: 'management@capgemini.com',
+    phone: '+91 95963214587',
+    contact: 'Aisha Keer',
+    sites: '6',
+    facilitator: '--',
+    tenants: '--',
+    tgroups: '--',
+  },
+
+  {
+    key: '4',
+    name: 'Lodhi',
+    email: 'management@lodhi.com',
+    phone: '+91 95963214587',
+    contact: 'Kanika Khare',
+    sites: '24',
+    facilitator: '--',
+    tenants: '16',
+    tgroups: '--',
+  },
+  {
+    key: '5',
+    name: 'Tata Consultancy Services',
+    email: 'management@tcs.com',
+    phone: '+91 95963214587',
+    contact: 'Prabhat Rao',
+    sites: '10',
+    facilitator: '--',
+    tenants: '--',
+    tgroups: '--',
+  },
+  {
+    key: '6',
+    name: 'HCL',
+    email: 'management@hcl.com',
+    phone: '+91 95963214587',
+    contact: 'Venkant Pant',
+    sites: '5',
+    facilitator: '--',
+    tenants: '2',
+    tgroups: '1',
+  },
+  {
+    key: '7',
+    name: 'Redingtion',
+    email: 'management@redingtion.com',
+    phone: '+91 95963214587',
+    contact: 'Samir Yadav',
+    sites: '1',
+    facilitator: '--',
+    tenants: '1',
+    tgroups: '--',
+  },
+  {
+    key: '8',
+    name: 'Mphasis',
+    email: 'management@mphasis.com',
+    phone: '+91 95963214587',
+    contact: 'Sweta Chawla',
+    sites: '1',
+    facilitator: '--',
+    tenants: '--',
+    tgroups: '--',
+  },
+
+  {
+    key: '9',
+    name: 'Larsen & Toubro',
+    email: 'management@larsentourbo.com',
+    phone: '+91 95963214587',
+    contact: 'Abhishek Kumar',
+    sites: '16',
+    facilitator: '--',
+    tenants: '--',
+    tgroups: '--',
   },
 ];
 
 const Clients = () => {
+  const [fixedTop, setFixedTop] = useState(false);
   return (
     <section>
-      <div className='mx-3 '>
+      <div className='mx-3  nunito'>
         <HeaderOfTable />
-        <Table columns={columns} dataSource={data} />
+        <Table
+          columns={columns}
+          dataSource={data}
+          scroll={{
+            x: 1300,
+          }}
+          /*  summary={() => (
+            <Table.Summary fixed={fixedTop ? 'top' : 'bottom'}>
+              {/* <Table.Summary.Row>
+                <Table.Summary.Cell index={0} colSpan={2}>
+                  <Switch
+                    checkedChildren='Fixed Top'
+                    unCheckedChildren='Fixed Top'
+                    checked={fixedTop}
+                    onChange={() => {
+                      setFixedTop(!fixedTop);
+                    }}
+                  />
+                </Table.Summary.Cell>
+                <Table.Summary.Cell index={2} colSpan={8}>
+                  Scroll Context
+                </Table.Summary.Cell>
+                <Table.Summary.Cell index={10}>Fix Right</Table.Summary.Cell>
+              </Table.Summary.Row> */
+          // </Table.Summary>
+          // )}
+          // sticky */
+        />
       </div>
     </section>
   );
